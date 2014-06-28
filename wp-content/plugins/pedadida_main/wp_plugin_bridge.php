3abd954676da7b6998effbcb1a2e5f79161c83e8
dolphincool16 <dolphincool16@gmail.com>
2014-06-28 16:08:48 -0400

    Bridge Plugin Cleanup

--- wp-content/plugins/pedadida_main/wp_plugin_bridge.php
+++ wp-content/plugins/pedadida_main/wp_plugin_bridge.php
@@ -21,16 +21,43 @@ You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-// INCLUDE ALL FILES
 
-require_once('login_form.php');
 
-// INCLUDE LOGIN FORM CUSTOMIZATION HERE
 
-// THIS AREA CHECKS IF USER HAS AN ACCOUNT
 
-	//THIS AREA CHECKS TO SEE IF YOU ARE LOGGED IN
+// LOGIN FORM CUSTOMIZATION INCLUDED
+require_once('registration_form.php');
+
+    // REGISTERING AREA
+	
+			//THIS IS THE AREA FOR WP CREATE ACCOUNT HOOK
+	add_action('user_register', 'pedadida_bridges_create');
+    function pedadida_bridges_create ($user_id) {
+    
+			//THIS IS THE AREA FOR MOODLE HOOK
+
+			//THIS IS THE AREA FOR RESOURCESPACE HOOK
+
+			//THIS IS THE AREA FOR SMF FORUM HOOOK
+
+			//THIS IS THE AREA FOR FENGOFFICE HOOK
+
+			//THIS IS THE AREA FOR SOCIAL ENGINE
+    
+		//THIS IS THE AREA TO SEND THE WELCOME MESSAGE
+    }
+    
+			//DO THEY NEED A CREDIT CARD
+				//THIS IS THE AREA IF THEY HAVE A CREDIT CARD
+				
+				//THIS IS THE AREA IF THEY DO NOT HAVE A CREDIT CARD
+				
+				
+	//LOGGING IN AREA
 			//IF THEY ARE NOT LOGGED IN
+			add_action('wp_login', 'pedadida_logins');
+			function pedadida_logins() {
+
 				//THIS IS THE AREA FOR THE WP LOGIN HOOK
 
 				//THIS IS THE AREA FOR MOODLE HOOK
@@ -42,7 +69,8 @@ require_once('login_form.php');
 				//THIS IS THE AREA FOR FENGOFFICE HOOK
 
 				//THIS IS THE AREA FOR SOCIAL ENGINE
-			
+			       
+            }
 			//IF THEY ARE LOGGED IN
 				//DO THEY NEED A CREDIT CARD
 				   require_once('credit_card.php');
@@ -53,29 +81,5 @@ require_once('login_form.php');
 
 
 
-
-	//IF THEY DO NOT HAVE AN ACCOUNT
-
-
-		//THIS IS THE AREA TO CREATE AN ACCOUNT
-		// test
-		
-			//THIS IS THE AREA FOR WP CREATE ACCOUNT HOOK
-			
-			
-			//THIS IS THE AREA FOR MOODLE HOOK
-
-			//THIS IS THE AREA FOR RESOURCESPACE HOOK
-
-			//THIS IS THE AREA FOR SMF FORUM HOOOK
-
-			//THIS IS THE AREA FOR FENGOFFICE HOOK
-
-			//THIS IS THE AREA FOR SOCIAL ENGINE
-		
-		//THIS IS THE AREA TO SEND THE WELCOME MESSAGE
-			//DO THEY NEED A CREDIT CARD
-				//THIS IS THE AREA IF THEY HAVE A CREDIT CARD
-				
-				//THIS IS THE AREA IF THEY DO NOT HAVE A CREDIT CARD
+    
 				
