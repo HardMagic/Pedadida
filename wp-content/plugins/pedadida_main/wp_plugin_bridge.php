3141a9efe6030fd4e16a5a94d9a8fe541d595bdd
Matt Hackney <matt@themov.org>
2014-06-28 06:13:44 -0600

    Rename wp_plugin_bridge to wp_plugin_bridge.php

---
+++ wp-content/plugins/pedadida_main/wp_plugin_bridge.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Plugin Name: Pedadida Single SignOn
+ * Plugin URI: http://pedadida.com/ssi
+ * Description: Single Sign On for Pedadida.
+ * Version: 1.0
+ * Author: Krashnik
+ * Author URI: http://modelmatt.com
+ * License: GPL2
+ */
+
+/*  Copyright 2014 Krashnik (email : info@hardmagic.com)
+This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+// INCLUDE ALL FILES
+// INCLUDE LOGIN FORM CUSTOMIZATION HERE
+
+// THIS AREA CHECKS IF USER HAS AN ACCOUNT
+
+	//THIS AREA CHECKS TO SEE IF YOU ARE LOGGED IN
+			//IF THEY ARE NOT LOGGED IN
+				//THIS IS THE AREA FOR THE WP LOGIN HOOK
+
+				//THIS IS THE AREA FOR MOODLE HOOK
+
+				//THIS IS THE AREA FOR RESOURCESPACE HOOK
+
+				//THIS IS THE AREA FOR SMF FORUM HOOOK
+
+				//THIS IS THE AREA FOR FENGOFFICE HOOK
+
+				//THIS IS THE AREA FOR SOCIAL ENGINE
+			
+			//IF THEY ARE LOGGED IN
+				//DO THEY NEED A CREDIT CARD
+
+					//THIS IS THE AREA IF THEY HAVE A CREDIT CARD
+					
+					//THIS IS THE AREA IF THEY DO NOT HAVE A CREDIT CARD
+
+
+
+
+	//IF THEY DO NOT HAVE AN ACCOUNT
+
+
+		//THIS IS THE AREA TO CREATE AN ACCOUNT
+		
+		
+			//THIS IS THE AREA FOR WP CREATE ACCOUNT HOOK
+			
+			
+			//THIS IS THE AREA FOR MOODLE HOOK
+
+			//THIS IS THE AREA FOR RESOURCESPACE HOOK
+
+			//THIS IS THE AREA FOR SMF FORUM HOOOK
+
+			//THIS IS THE AREA FOR FENGOFFICE HOOK
+
+			//THIS IS THE AREA FOR SOCIAL ENGINE
+		
+		//THIS IS THE AREA TO SEND THE WELCOME MESSAGE
+			//DO THEY NEED A CREDIT CARD
+				//THIS IS THE AREA IF THEY HAVE A CREDIT CARD
+				
+				//THIS IS THE AREA IF THEY DO NOT HAVE A CREDIT CARD
+				
